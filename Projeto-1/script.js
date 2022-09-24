f820a2a7a69b463d8330670242a9a95b7a026581
Rayene Amaro <96782425+RayeneAmaro@users.noreply.github.com>
2022-09-24 19:22:32 -0300

    Delete Projeto-1 directory

--- Projeto-1/script.js
+++
@@ -1,6 +0,0 @@
-const alterarLinks = document.querySelectorAll('a[href^="www.google.com.br"]');
-
-alterarLinks.forEach((alterar) => {
-    alterar.setAttribute('href', 'https://www.google.com/');
-})
-
