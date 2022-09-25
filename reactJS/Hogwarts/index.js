fd193443ba9c0b89b74b9322200bfee89abb8515
Rayene Amaro <96782425+RayeneAmaro@users.noreply.github.com>
2022-09-25 10:04:22 -0300

    Add files via upload

---
+++ reactJS/Hogwarts/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HarryPotter from './projeto/HarryPotter';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HarryPotter/>
+  </React.StrictMode>
+);
+
