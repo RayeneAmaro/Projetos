a929ca720d3ab19d54974cac8899921edbc72695
Rayene Amaro <96782425+RayeneAmaro@users.noreply.github.com>
2022-09-29 18:15:01 -0300

    Update Erro.js

--- reactJS/Filmes - Rotas/pages/Erro.js
+++ reactJS/Filmes - Rotas/pages/Erro.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Erro.css'
 
 function Erro() {
   return (
-    <div>
-      <h1>Página não encontrada</h1>
-      <Link to='/'>Voltar</Link>
+    <div className='divErro'>
+      <h1 className='erro'>Página não encontrada</h1>
+      <Link to='/' className='linkErro'>Voltar</Link>
     </div>
   )
 }
